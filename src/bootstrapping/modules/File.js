bea430842d8304a9a8e3e01d9d9d3243f5e7204f
Jacob G. Acosta <jacob.gacosta@gmail.com>
2021-01-03 22:39:02 -0600

    Add loader to file-object rules
    Now the fonts are resolved

--- src/bootstrapping/modules/File.js
+++ src/bootstrapping/modules/File.js
@@ -20,6 +20,19 @@ class File {
                     }
                 }
             },
+            {
+                test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            publicPath: '../webfonts',
+                            outputPath: "webfonts",
+                        }
+                    }
+                ]
+            }
         ]
     }
 
